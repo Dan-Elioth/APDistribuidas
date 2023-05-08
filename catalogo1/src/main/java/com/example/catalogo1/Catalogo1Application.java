8cbaedb7576d869b54d3309d88360a562c80a7f2
Dan-Elioth <elitocp@gmail.com>
2023-05-08 10:03:01 -0500

    Catalogo prueba

---
+++ catalogo1/src/main/java/com/example/catalogo1/Catalogo1Application.java
@@ -0,0 +1,13 @@
+package com.example.catalogo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Catalogo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Catalogo1Application.class, args);
+    }
+
+}
---
+++ catalogo1/src/test/java/com/catalogoweb/catalogo1/Catalogo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogoweb.catalogo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Catalogo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
